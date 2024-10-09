817ef3d620c4af6d53af44e413310650d2fb5d4d
LeThai98 <hongthaifamily@gmail.com>
2024-10-09 08:58:33 +0700

    call API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,25 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api';
+  private http = inject(HttpClient);
+  products: Product[] = [];
+  title = 'EThaiShop'; 
+  constructor() {}
+  ngOnInit() {
+    this.http.get<Pagination<Product>>(this.baseUrl + '/product').subscribe({
+      next: response => {
+        this.products =  response.data;
+        console.log(this.products);
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      },
+      complete: () => {
+        
+      }
+    });
+  }
+  
 }
