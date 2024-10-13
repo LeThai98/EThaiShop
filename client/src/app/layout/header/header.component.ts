0d3f282de0be77f84d8b7b890e172c2742f56b3c
LeThai98 <hongthaifamily@gmail.com>
2024-10-13 16:34:16 +0700

    Interceptor & Toasting & process bar & fixed header to the top

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon} from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
@@ -13,11 +15,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     MatBadge,
     RouterOutlet, 
     RouterLink, 
-    RouterLinkActive
+    RouterLinkActive,
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  busyService = inject(BusyService);
 }
